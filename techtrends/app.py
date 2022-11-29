5927e3ae91ca47b2710b15525253be28a0f4aaca
Chijioke Eze <cc.eze90@gmail.com>
2022-11-29 06:54:15 +0100

    adds Dockerfile, modifies app.py and docker_commands

--- techtrends/app.py
+++ techtrends/app.py
@@ -1,13 +1,26 @@
 import sqlite3
-
+import logging
+import sys 
+import time
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
+from werkzeug.exceptions import abort
+
+connection_count = 0
+
+# Define logger message display functionality
+def logger_message(message):
+    date = time.asctime() 
+    return f"{date}, {message}"
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    global connection_count
+    connection_count += 1
     return connection
 
 # Function to get a post using its ID
@@ -65,6 +78,37 @@ def create():
 
     return render_template('create.html')
 
+# Define the healthz functionality
+@app.route('/healthz')
+def healthz():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+    app.logger.debug(logger_message('Test debug message'))
+    return response
+
+# Define the metrics functionlaity
+@app.route('/metrics')
+def metrics():
+    connection = get_db_connection()
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+    connection.close()
+    response = app.response_class(
+            response=json.dumps({"db_connection_count": connection_count, "post_count": len(posts)}),
+            status=200,
+            mimetype='application/json'
+    )
+    return response
+
 # start the application on port 3111
 if __name__ == "__main__":
+   logging.basicConfig(
+    level=logging.DEBUG,
+    handlers=[
+        logging.StreamHandler(sys.stdout),
+        logging.StreamHandler(sys.stderr)
+        ]
+    )
    app.run(host='0.0.0.0', port='3111')
